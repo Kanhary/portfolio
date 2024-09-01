560226a33d363cdd53a8bcf2a18fe5ca08eda6d6
Sem Pheakdey <sempheakdey417@gmail.com>
2024-09-01 20:45:25 +0700

    Update Bounce animate about dropdown icon

--- src/Components/Pages/employee/TabMenu.jsx
+++ src/Components/Pages/employee/TabMenu.jsx
@@ -408,7 +408,7 @@ const TabMenu = () => {
                     {activeTab === 'tab2' && 'បញ្ចូលវគ្គសិក្សារយៈពេលវែង'}
                     {activeTab === 'tab3' && 'បញ្ចូលវគ្គសិក្សារយៈពេលខ្លី'}
                   </span>
-                  <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <svg className="w-4 h-4 ml-2 animate-bounce text-blue-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
                   </svg>
                 </button>
