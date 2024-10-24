fe717572c0d1d91745b929cef287d688be2ac6c3
Kagnary <128480273+Kanhary@users.noreply.github.com>
2024-10-24 15:59:41 +0700

    update

--- src/utils/requst.js
+++ src/utils/requst.js
@@ -1,24 +1,44 @@
 import axios from 'axios';
+import { getToken } from './token/Token';
 
 const request = axios.create({
     baseURL: 'http://192.168.168.14:8888',
     withCredentials: false,
-    timeout: 30000 
-})
+    timeout: 30000,
+});
 
-axios.defaults.headers['Content-Type'] = 'application/json;chatset=utf-8'
-request.interceptors.response.use((response) => {
-    console.log(response.data)
-    let {msg,code} = response.data
-    console.log("code=====>",code,'msg====>',msg);
-    if(code == null) {
-        return response;
-    }else if(code == 200) {
-        return response;
+// Set default headers
+axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8';
+
+// Request interceptor for adding token
+request.interceptors.request.use(
+    (config) => {
+        const token = getToken('yourTokenKey'); // Replace with your token key
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return Promise.reject(msg);
-},(error) => {
-    return Promise.reject(error);
-})
+);
+
+// Response interceptor
+request.interceptors.response.use(
+    (response) => {
+        console.log(response.data);
+        const { msg, code } = response.data;
+        console.log("code=====>", code, 'msg====>', msg);
+        if (code == null || code === 200) {
+            return response;
+        }
+        return Promise.reject(msg);
+    },
+    (error) => {
+        console.error('Error Response:', error.response);
+        return Promise.reject(error);
+    }
+);
 
 export default request;
